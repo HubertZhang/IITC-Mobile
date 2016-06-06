a9462133f88a07c3343a6b777f6a17dc44743c55
Hubertzhang <hubert.zyk@gmail.com>
2016-06-07 01:26:24 +0800

    add JSHandler

---
+++ IITC-Mobile/IITCWebView/JSHandler.swift
@@ -0,0 +1,13 @@
+//
+//  JSHandler.swift
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 16/6/7.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+import UIKit
+
+class JSHandler: WKScriptMessageHandler {
+
+}
