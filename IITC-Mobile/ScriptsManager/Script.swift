fb735d8b6b067917e3af609e49016f379fd9ec17
Hubertzhang <hubert.zyk@gmail.com>
2016-06-07 01:05:00 +0800

    add scripts manager

---
+++ IITC-Mobile/ScriptsManager/Script.swift
@@ -0,0 +1,13 @@
+//
+//  Script.swift
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 16/6/6.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+import UIKit
+
+class Script: NSObject {
+
+}
---
+++ IITC-Mobile/ScriptsManager/ScriptsManager.swift
@@ -0,0 +1,13 @@
+//
+//  ScriptsManager.swift
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 16/6/6.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+import UIKit
+
+class ScriptsManager: NSObject {
+
+}
