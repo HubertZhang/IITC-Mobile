8ca30738e748e3bf9277d768aec1d14fdc6feaff
Hubertzhang <hubert.zyk@gmail.com>
2016-06-06 23:51:11 +0800

    init project

---
+++ IITC-MobileTests/IITC_MobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  IITC_MobileTests.swift
+//  IITC-MobileTests
+//
+//  Created by Hubert Zhang on 16/6/6.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+import XCTest
+@testable import IITC_Mobile
+
+class IITC_MobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
