412dffe546e802ae82bf7a0c6901e28a6e54f2b9
Hubertzhang <hubert.zyk@gmail.com>
2016-06-07 13:34:31 +0800

    update scripts manager

--- IITC-Mobile/ScriptsManager/Script.swift
+++ IITC-Mobile/ScriptsManager/Script.swift
@@ -9,65 +9,69 @@
 import UIKit
 
 class Script: NSObject {
-    var fileName : String
-    var version : String?
-    var name : String?
-    var category : String?
-    var scriptDescription : String?
-    var filePath : NSURL
-    var downloadURL : String?
-    var updateURL : String?
-    lazy var fileContent : String = {
-        return try! String(contentsOfURL:self.filePath, encoding:  NSUTF8StringEncoding)
-    }()
-    
-    init(atFilePath filePath:NSURL) {
-        let fileContent = String(fileContent:filePath, encoding:  NSUTF8StringEncoding)
+    var fileName: String
+    var version: String?
+    var name: String?
+    var category: String
+    var scriptDescription: String?
+    var filePath: NSURL
+    var downloadURL: String?
+    var updateURL: String?
+    var fileContent: String
+
+    init(coreJS filePath: NSURL, withName name: String) throws {
+        self.fileContent = try String(contentsOfURL: filePath)
+        self.name = name
+        self.filePath = filePath;
+        self.fileName = filePath.lastPathComponent!
+        self.category = "Core"
+        super.init()
+    }
+
+    init(atFilePath filePath: NSURL) throws {
+        self.fileContent = try String(contentsOfURL: filePath)
         let attributes = Script.getJSAttributes(fileContent)
         self.version = attributes["version"];
         self.updateURL = attributes["updateURL"];
         self.downloadURL = attributes["downloadURL"];
         self.name = attributes["name"];
-        self.category = attributes["category"];
-        if (self.category == nil) {
-            self.category = "Undefined";
-        }
+        self.category = attributes["category"] ?? "Undefined"
         self.scriptDescription = attributes["description"];
         self.filePath = filePath;
         self.fileName = filePath.lastPathComponent!
         super.init()
     }
-    
-    static func getJSAttributes(fileContent:String) -> Dictionary<String, String> {
+
+    static func getJSAttributes(fileContent: String) -> Dictionary<String, String> {
         var attributes = Dictionary<String, String>()
-        
+
         do {
             let range1 = fileContent.rangeOfString("==UserScript==")
             let range2 = fileContent.rangeOfString("==/UserScript==")
-            
-            var e:NSRegularExpression
+
+            var e: NSRegularExpression
             e = try NSRegularExpression(pattern: "//.*?@([^\\s]*)\\s*(.*)", options: NSRegularExpressionOptions(rawValue: 0))
             let header = fileContent.substringWithRange(Range<String.Index>(start: (range1?.endIndex)!.successor(), end: (range2?.startIndex)!))
             for line in header.componentsSeparatedByString("\n") {
-                print(line)
-                let search = e.matchesInString(line, options: NSMatchingOptions(rawValue: 0), range:NSMakeRange(0, line.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)))
-                if (search.count>0) {
-                    print(search[0].rangeAtIndex(1))
-                    print(search[0].rangeAtIndex(2))
-                    var start = line.startIndex.advancedBy(search[0].rangeAtIndex(1).location )
-                    var end = start.advancedBy(search[0].rangeAtIndex(1).length-1)
+//                print(line)
+                let search = e.matchesInString(line, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, (line as NSString).length))
+                if (search.count > 0) {
+//                    print(search[0].rangeAtIndex(1))
+//                    print(search[0].rangeAtIndex(2))
+                    var start = line.startIndex.advancedBy(search[0].rangeAtIndex(1).location)
+                    var end = start.advancedBy(search[0].rangeAtIndex(1).length - 1)
                     let rangeId = line[start ... end]
                     start = line.startIndex.advancedBy(search[0].rangeAtIndex(2).location)
-                    end = start.advancedBy(search[0].rangeAtIndex(2).length-1)
+                    end = start.advancedBy(search[0].rangeAtIndex(2).length - 1)
                     let rangeDetail = line[start ... end]
-                    attributes[rangeId]=rangeDetail
+                    attributes[rangeId] = rangeDetail
                 }
             }
         } catch _ as NSError {
-            
+
         }
-        
-        print(attributes)
+
+//        print(attributes)
         return attributes
     }
 
--- IITC-Mobile/ScriptsManager/ScriptsManager.swift
+++ IITC-Mobile/ScriptsManager/ScriptsManager.swift
@@ -9,40 +9,43 @@
 import UIKit
 
 class ScriptsManager: NSObject {
-    let sharedInstance = ScriptsManager()
-    
+    static let sharedInstance = ScriptsManager()
+
     var storedPlugins = [Script]()
-    var loadedPluginNames : [String]
-    
-    var mainScript : Script
-    var hookScript : Script
-    var positionScript : Script
-    
-    var initialScriptsPath : NSURL
-    var initialPluginsPath : NSURL
-    var userScriptsPath : NSURL
-    var userPluginsPath : NSURL
-    
+    var loadedPluginNames: [String]
+    var loadedPlugins: Set<String>
+
+    var mainScript: Script
+    var hookScript: Script
+    var positionScript: Script
+
+    var initialScriptsPath: NSURL
+    var initialPluginsPath: NSURL
+    var userScriptsPath: NSURL
+    var userPluginsPath: NSURL
+
     override init() {
         initialScriptsPath = NSBundle.mainBundle().resourceURL!.URLByAppendingPathComponent("scripts", isDirectory: true)
         initialPluginsPath = initialScriptsPath.URLByAppendingPathComponent("plugins", isDirectory: true)
         userScriptsPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last!
         userPluginsPath = userScriptsPath.URLByAppendingPathComponent("plugins", isDirectory: true)
-        mainScript = Script(atFilePath: initialScriptsPath.URLByAppendingPathComponent("total-conversion-build.user.js"))
-        hookScript = Script(atFilePath: initialScriptsPath.URLByAppendingPathComponent("ios-hooks.js"))
-        positionScript = Script(atFilePath: initialScriptsPath.URLByAppendingPathComponent("user-location.user.js"))
+        mainScript = try! Script(atFilePath: initialScriptsPath.URLByAppendingPathComponent("total-conversion-build.user.js"))
+        hookScript = try! Script(coreJS: initialScriptsPath.URLByAppendingPathComponent("ios-hooks.js"), withName: "hook")
+        hookScript.fileContent = String(format: hookScript.fileContent, "1.0", 20)
+        positionScript = try! Script(coreJS: initialScriptsPath.URLByAppendingPathComponent("user-location.user.js"), withName: "position")
         loadedPluginNames = NSUserDefaults.standardUserDefaults().arrayForKey("LoadedPlugins") as? [String] ?? [String]()
-        
+        loadedPlugins = Set<String>(loadedPluginNames)
         super.init()
         loadUserMainScript()
         loadAllPlugins()
-        
+
     }
-    
+
     func loadAllPlugins() {
         self.storedPlugins = loadPluginInDirectory(initialPluginsPath)
         for plugin in loadPluginInDirectory(userPluginsPath) {
-            let index = storedPlugins.indexOf{ oldPlugin -> Bool in
+            let index = storedPlugins.indexOf {
+                oldPlugin -> Bool in
                 return oldPlugin.fileName == plugin.fileName
             }
             if index != nil {
@@ -52,30 +55,43 @@ class ScriptsManager: NSObject {
             }
         }
     }
-    
+
     func loadUserMainScript() {
         let userURL = userScriptsPath.URLByAppendingPathComponent("total-conversion-build.user.js")
         if NSFileManager.defaultManager().fileExistsAtPath(userURL.path!) {
-            mainScript = Script(atFilePath:userURL)
+            do {
+                mainScript = try Script(atFilePath: userURL)
+            } catch {
+
+            }
         }
     }
-    
-    func loadPluginInDirectory(url:NSURL) -> [Script] {
+
+    func loadPluginInDirectory(url: NSURL) -> [Script] {
+        try! NSFileManager.defaultManager().createDirectoryAtURL(url, withIntermediateDirectories: true, attributes: nil)
         let directoryContents = try! NSFileManager.defaultManager().contentsOfDirectoryAtURL(url, includingPropertiesForKeys: nil, options: .SkipsHiddenFiles)
         var result = [Script]()
         for pluginPath in directoryContents {
             if pluginPath.path!.hasSuffix(".meta.js") {
                 continue
             }
-            result.append(Script(atFilePath: pluginPath))
+            do {
+                try result.append(Script(atFilePath: pluginPath))
+            } catch {
+                continue
+            }
         }
         return result
     }
-    
-    func getLoadedPlugins() -> [Script] {
+
+    func getLoadedScripts() -> [Script] {
         var result = [Script]()
+        result.append(mainScript)
+        result.append(hookScript)
+        result.append(positionScript)
         for name in loadedPluginNames {
-            let index = storedPlugins.indexOf{ plugin -> Bool in
+            let index = storedPlugins.indexOf {
+                plugin -> Bool in
                 return plugin.fileName == name
             }
             if index != nil {
@@ -84,9 +100,10 @@ class ScriptsManager: NSObject {
         }
         return result
     }
-    
-    func setPlugin(script:Script, loaded : Bool) {
-        let index = loadedPluginNames.indexOf{ plugin -> Bool in
+
+    func setPlugin(script: Script, loaded: Bool) {
+        let index = loadedPluginNames.indexOf {
+            plugin -> Bool in
             return plugin == script.fileName
         }
         if index != nil && !loaded {
@@ -94,10 +111,11 @@ class ScriptsManager: NSObject {
         } else if index == nil && loaded {
             loadedPluginNames.append(script.fileName)
         }
+        loadedPlugins = Set<String>(loadedPluginNames)
         NSUserDefaults.standardUserDefaults().setObject(loadedPluginNames, forKey: "LoadedPlugins")
     }
 
     func updatePlugins() {
-        
+
     }
 }
