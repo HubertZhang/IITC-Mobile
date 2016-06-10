a30600b9ddfec27d11aa5d33f4c84dfd13606c17
Hubertzhang <hubert.zyk@gmail.com>
2016-06-10 18:47:54 +0800

    use framework for action widget

---
+++ BaseFramework/BaseFramework.h
@@ -0,0 +1,19 @@
+//
+//  BaseFramework.h
+//  BaseFramework
+//
+//  Created by Hubert Zhang on 16/6/10.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseFramework.
+FOUNDATION_EXPORT double BaseFrameworkVersionNumber;
+
+//! Project version string for BaseFramework.
+FOUNDATION_EXPORT const unsigned char BaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseFramework/PublicHeader.h>
+
+
