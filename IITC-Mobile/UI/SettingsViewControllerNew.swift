7c791caa5048c155caa62c79a087e8c1438a5c32
Hubertzhang <hubert.zyk@gmail.com>
2016-06-29 03:01:20 +0800

    add an advanced menu for downloading test build

--- IITC-Mobile/UI/SettingsViewControllerNew.swift
+++ IITC-Mobile/UI/SettingsViewControllerNew.swift
@@ -13,19 +13,20 @@ import RxCocoa
 import RxBlocking
 import InAppSettingsKit
 import MBProgressHUD
+import Alamofire
 
 @objc class SettingsViewController: IASKAppSettingsViewController, IASKSettingsDelegate {
 
     override func viewDidLoad() {
         self.delegate = self
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         let tracker = GAI.sharedInstance().defaultTracker
         tracker.set(kGAIScreenName, value: "Settings")
-        
+
         let builder = GAIDictionaryBuilder.createScreenView()
-        tracker.send(builder.build() as [NSObject : AnyObject])
+        tracker.send(builder.build() as [NSObject:AnyObject])
     }
 
     override init(style: UITableViewStyle) {
@@ -33,11 +34,11 @@ import MBProgressHUD
         let defaults = NSUserDefaults(suiteName: ContainerIdentifier)
         self.settingsStore = IASKSettingsStoreUserDefaults(userDefaults: defaults)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     func settingsViewController(sender: IASKAppSettingsViewController!, buttonTappedForSpecifier specifier: IASKSpecifier!) {
         if (specifier.key() == "pref_plugins") {
             let vc = self.navigationController!.storyboard!.instantiateViewControllerWithIdentifier("pluginsViewController")
@@ -71,6 +72,50 @@ import MBProgressHUD
         } else if specifier.key() == "pref_new" {
             let vc = self.navigationController!.storyboard!.instantiateViewControllerWithIdentifier("whatsNewViewController")
             self.navigationController!.pushViewController(vc, animated: true)
+        } else if specifier.key() == "pref_download_test" {
+            let alert = UIAlertController(title: "Download test build", message: "Warning: download script may override IITC script you added. Continue?", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {
+                action in
+                let hud = MBProgressHUD.showHUDAddedTo(self.navigationController!.view, animated: true);
+                hud.mode = MBProgressHUDMode.AnnularDeterminate;
+                hud.labelText = "Downloading IITC script...";
+
+                Alamofire.download(.GET, "https://secure.jonatkins.com/iitc/test/total-conversion-build.user.js", destination: {
+                    (url, response) -> NSURL in
+                    let pathComponent = response.suggestedFilename
+                    let downloadPath = ScriptsManager.sharedInstance.userScriptsPath.URLByAppendingPathComponent("total-conversion-build.user.js")
+                    if NSFileManager.defaultManager().fileExistsAtPath(downloadPath.path!) {
+                        do {
+                            try NSFileManager.defaultManager().removeItemAtPath(downloadPath.path!)
+                        } catch {
+
+                        }
+                    }
+                    return downloadPath
+                }).progress {
+                    bytesRead, totalBytesRead, totalBytesExpectedToRead in
+
+                    // This closure is NOT called on the main queue for performance
+                    // reasons. To update your ui, dispatch to the main queue.
+                    dispatch_async(dispatch_get_main_queue()) {
+                        hud.progress = Float(totalBytesRead) / Float(totalBytesExpectedToRead)
+                    }
+                }.response {
+                    request, response, _, error in
+
+                    hud.hide(true)
+                    if error != nil {
+                        let alert1 = UIAlertController(title: "Error", message: error!.description, preferredStyle: .Alert)
+                        alert1.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+                        self.presentViewController(alert1, animated: true, completion: nil)
+                    }
+                }
+
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+
+            self.presentViewController(alert, animated: true, completion: nil)
+
         }
     }
 
