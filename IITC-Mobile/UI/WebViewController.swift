9c5ae75fc2e5f20b433ff0c2cd7830a916393983
Hubertzhang <hubert.zyk@gmail.com>
2016-09-30 21:04:19 +0800

    Implement WebViewController

--- IITC-Mobile/UI/WebViewController.swift
+++ IITC-Mobile/UI/WebViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    var configuration: WKWebViewConfiguration?
+    var webView: WKWebView!
+    @IBOutlet weak var webProgressView: UIProgressView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if configuration != nil {
+            self.webView = WKWebView(frame: CGRect.zero, configuration: configuration!)
+        } else {
+            self.webView = WKWebView(frame: CGRect.zero)
+        }
+        
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.uiDelegate = self
+        self.view.addSubview(self.webView);
+        
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(NSLayoutConstraint.init(item: self.topLayoutGuide, attribute: .bottom, relatedBy: .equal, toItem: self.webView, attribute: .top, multiplier: 1.0, constant: 0))
+        constraints.append(NSLayoutConstraint.init(item: self.bottomLayoutGuide, attribute: .top, relatedBy: .equal, toItem: self.webView, attribute: .bottom, multiplier: 1.0, constant: 0))
+        constraints.append(NSLayoutConstraint.init(item: self.view, attribute: .leading, relatedBy: .equal, toItem: self.webView, attribute: .leading, multiplier: 1.0, constant: 0))
+        constraints.append(NSLayoutConstraint.init(item: self.view, attribute: .trailing, relatedBy: .equal, toItem: self.webView, attribute: .trailing, multiplier: 1.0, constant: 0))
+        self.view.addConstraints(constraints)
+        self.view.bringSubview(toFront: webProgressView)
+        
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        self.webView.addObserver(self, forKeyPath: "title", options: .new, context: nil)
         // Do any additional setup after loading the view.
+    }
+    
+    deinit {
+        self.webView.removeObserver(self, forKeyPath: "estimatedProgress")
+        self.webView.removeObserver(self, forKeyPath: "title")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +50,77 @@ class WebViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey:Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            let progress: Double = self.webView.estimatedProgress
+            self.webProgressView.setProgress(Float(progress), animated: true)
+            if progress == 1.0 {
+                UIView.animate(withDuration: 1, animations: {
+                    () -> Void in
+                    self.webProgressView.alpha = 0
+                    }, completion: { result in
+                        self.webProgressView.progress = 0
+                })
+            } else {
+                self.webProgressView.alpha = 1
+            }
+        } else if (keyPath == "title") {
+            self.title = self.webView.title
+        }
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController: UIAlertController = UIAlertController(title: message, message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .cancel, handler: {
+            (action: UIAlertAction) -> Void in
+            completionHandler()
+        }))
+        self.present(alertController, animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController: UIAlertController = UIAlertController(title: message, message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: {
+            (action: UIAlertAction) -> Void in
+            completionHandler(true)
+        }))
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: {
+            (action: UIAlertAction) -> Void in
+            completionHandler(false)
+        }))
+        self.present(alertController, animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: prompt, message: webView.url!.host, preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: {
+            (textField: UITextField) -> Void in
+            textField.text = defaultText
+        })
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: {
+            (action: UIAlertAction) -> Void in
+            let input = alertController.textFields!.first!.text!
+            completionHandler(input)
+        }))
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: {
+            (action: UIAlertAction) -> Void in
+            completionHandler(nil)
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webViewDidClose(_ webView: WKWebView) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeButtonClicked(_:AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
 
     /*
     // MARK: - Navigation
