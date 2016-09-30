83b38ef0a88856148c8ef27c3793454026f5fb7b
Hubertzhang <hubert.zyk@gmail.com>
2016-09-30 21:03:35 +0800

    Add WebViewController for popups

---
+++ IITC-Mobile/UI/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 2016/9/30.
+//  Copyright © 2016年 IITC. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
