fa0c11672cea995758f90014c5025f561d362d7f
Hubert Zhang <hubert.zyk@gmail.com>
2017-08-09 20:15:48 +0800

    add StoreKit, add manager for IAP

---
+++ IITC-Mobile/InAppPurchaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseManager.swift
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 2017/7/23.
+//  Copyright © 2017年 IITC. All rights reserved.
+//
+
+import UIKit
+
+class InAppPurchaseManager: NSObject {
+
+}
