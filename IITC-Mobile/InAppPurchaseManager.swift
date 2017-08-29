21e6aeae1aea078aecf7e631dcbe87c528e467e8
Hubert Zhang <hubert.zyk@gmail.com>
2017-08-29 20:07:12 +0800

    implement IAP manager

--- IITC-Mobile/InAppPurchaseManager.swift
+++ IITC-Mobile/InAppPurchaseManager.swift
@@ -7,7 +7,84 @@
 //
 
 import UIKit
+import StoreKit
 
 class InAppPurchaseManager: NSObject {
-
+    static let `default` = InAppPurchaseManager()
+    
+    var uiDelegate: ConsolePurchaseViewController?
+    
+    override init() {
+        super.init()
+        #if DEBUG
+            let request = SKReceiptRefreshRequest(receiptProperties: nil)
+            request.delegate = self
+            request.start()
+        #else
+            if arc4random_uniform(10) == 0 {
+                let request = SKReceiptRefreshRequest(receiptProperties: nil)
+                request.delegate = self
+                request.start()
+            }
+        #endif
+    }
+    
+    func verifyReciept() {
+        let url = Bundle.main.appStoreReceiptURL
+        if url != nil && FileManager.default.fileExists(atPath: url!.path) {
+            if let receipt = NSData(contentsOf: url!) {
+                UserDefaults.standard.set(true, forKey: "Verified")
+            }
+        } else {
+            UserDefaults.standard.set(false, forKey: "Verified")
+        }
+    }
 }
+
+extension InAppPurchaseManager: SKPaymentTransactionObserver {
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        self.uiDelegate?.paymentQueue(queue, updatedTransactions: transactions)
+        for transaction in transactions {
+            switch (transaction.transactionState) {
+            case .purchasing:
+                print("Purchasing...")
+                break
+            case .deferred:
+                print("Deferred...")
+                break
+            case .failed:
+                print("Failed!")
+                break
+            case .purchased:
+                print("Purchased")
+                if transaction.payment.productIdentifier == "com.hubertzhang.iitcmobile.console" {
+                    UserDefaults.standard.set(true, forKey: "Purchased")
+                    UserDefaults.standard.set(transaction.transactionIdentifier, forKey: "TransactionID")
+                    self.verifyReciept()
+                }
+                queue.finishTransaction(transaction)
+                break
+            case .restored:
+                print("Restored")
+                if transaction.original?.payment.productIdentifier == "com.hubertzhang.iitcmobile.console" {
+                    UserDefaults.standard.set(true, forKey: "Purchased")
+                    UserDefaults.standard.set(transaction.original?.transactionIdentifier, forKey: "TransactionID")
+                    self.verifyReciept()
+                }
+                queue.finishTransaction(transaction)
+                break
+            }
+        }
+    }
+}
+
+extension InAppPurchaseManager: SKRequestDelegate {
+    func requestDidFinish(_ request: SKRequest) {
+        
+    }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print(request, error)
+    }
+}
+
