1ec4062a1646cd1f9cd72916d339e74b51dae9ac
Hubert Zhang <hubert.zyk@gmail.com>
2017-10-02 23:13:21 +0800

    update code in settings view

--- IITC-Mobile/UI/SettingsViewControllerNew.swift
+++ IITC-Mobile/UI/SettingsViewControllerNew.swift
@@ -71,17 +71,80 @@ import FirebaseAnalytics
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 
+    func pushViewController(withIdentifier identifier: String) {
+        guard let vc = self.navigationController?.storyboard?.instantiateViewController(withIdentifier: identifier) else {
+            return
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
     func settingsViewController(_ sender: IASKAppSettingsViewController!, buttonTappedFor specifier: IASKSpecifier!) {
-        if (specifier.key() == "pref_plugins") {
-            let vc = self.navigationController!.storyboard!.instantiateViewController(withIdentifier: "pluginsViewController")
-            self.navigationController!.pushViewController(vc, animated: true)
-        } else if (specifier.key() == "pref_update") {
-            let hud = MBProgressHUD.showAdded(to: self.navigationController!.view, animated: true)
-            hud.mode = MBProgressHUDMode.annularDeterminate
-            hud.label.text = "Updating..."
-            var finished = 0
-            let all = ScriptsManager.sharedInstance.storedPlugins.count + 2
-            ScriptsManager.sharedInstance.updatePlugins().subscribeOn(SerialDispatchQueueScheduler.init(internalSerialQueueName: "com.cradle.IITC-Mobile.network")).observeOn(MainScheduler.instance).subscribe(onNext: {
+        switch specifier.key() {
+        case "pref_plugins":
+            self.pushViewController(withIdentifier: "pluginsViewController")
+        case "pref_about":
+            self.pushViewController(withIdentifier: "aboutViewController")
+        case "pref_new":
+            self.pushViewController(withIdentifier: "whatsNewViewController")
+        case "pref_useragent_button":
+            self.pushViewController(withIdentifier: "userAgentViewController")
+        case "pref_console_not_purchased":
+            guard let vc = self.navigationController?.storyboard?.instantiateViewController(withIdentifier: "purchase") else {
+                return
+            }
+            vc.modalPresentationStyle = UIModalPresentationStyle.formSheet
+            self.present(vc, animated: true, completion: nil)
+        case "pref_update":
+            self.startUpdatePlugins()
+        case "pref_adv_download_test":
+            let alert = UIAlertController(title: "Download test build of IITC", message: "Warning: downloaded script may override IITC script you added. Continue?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: self.startDownloadIITCScript))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+            self.present(alert, animated: true, completion: nil)
+            break
+        default:
+            return
+        }
+    }
+
+    func startDownloadIITCScript(_ action: UIAlertAction) {
+        let hud = MBProgressHUD.showAdded(to: self.navigationController!.view, animated: true)
+        hud.mode = MBProgressHUDMode.annularDeterminate
+        hud.label.text = "Downloading IITC script..."
+
+        Alamofire.download("https://iitc.me/build/test/total-conversion-build.user.js", to: {
+            _, _ -> (destinationURL: URL, options: DownloadRequest.DownloadOptions) in
+            let downloadPath = ScriptsManager.sharedInstance.userScriptsPath.appendingPathComponent("total-conversion-build.user.js")
+            return (downloadPath, DownloadRequest.DownloadOptions.removePreviousFile)
+        }).downloadProgress(queue: DispatchQueue.main, closure: {
+            progress in
+
+            // This closure is NOT called on the main queue for performance
+            // reasons. To update your ui, dispatch to the main queue.
+            hud.progressObject = progress
+        }).response(queue: DispatchQueue.main, completionHandler: {
+            downloadResponse in
+
+            hud.hide(animated: true)
+            if downloadResponse.error != nil {
+                let alert1 = UIAlertController(title: "Error", message: downloadResponse.error!.localizedDescription, preferredStyle: .alert)
+                alert1.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert1, animated: true, completion: nil)
+            }
+        })
+    }
+
+    func startUpdatePlugins() {
+        let hud = MBProgressHUD.showAdded(to: self.navigationController!.view, animated: true)
+        hud.mode = MBProgressHUDMode.annularDeterminate
+        hud.label.text = "Updating..."
+        var finished = 0
+        let all = ScriptsManager.sharedInstance.storedPlugins.count + 2
+        ScriptsManager.sharedInstance.updatePlugins()
+            .subscribeOn(SerialDispatchQueueScheduler.init(internalSerialQueueName: "com.cradle.IITC-Mobile.network"))
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: {
                 _ -> Void in
                 finished += 1
                 hud.progress = Float(finished) / Float(all)
@@ -91,63 +154,16 @@ import FirebaseAnalytics
             }, onCompleted: {
                 () -> Void in
                 hud.label.text = "Scanning..."
-                ScriptsManager.sharedInstance.loadAllPlugins()
-                ScriptsManager.sharedInstance.loadUserMainScript()
-                hud.hide(animated: true)
+                DispatchQueue.global().async(execute: {
+                    ScriptsManager.sharedInstance.loadAllPlugins()
+                    ScriptsManager.sharedInstance.loadUserMainScript()
+                    DispatchQueue.main.async(execute: {
+                        hud.hide(animated: true)
+                    })
+                })
             }, onDisposed: {
                 () -> Void in
             })
-
-        } else if specifier.key() == "pref_about" {
-            let vc = self.navigationController!.storyboard!.instantiateViewController(withIdentifier: "aboutViewController")
-            self.navigationController!.pushViewController(vc, animated: true)
-        } else if specifier.key() == "pref_new" {
-            let vc = self.navigationController!.storyboard!.instantiateViewController(withIdentifier: "whatsNewViewController")
-            self.navigationController!.pushViewController(vc, animated: true)
-        } else if specifier.key() == "pref_adv_download_test" {
-            let alert = UIAlertController(title: "Download test build", message: "Warning: download script may override IITC script you added. Continue?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
-                _ in
-                let hud = MBProgressHUD.showAdded(to: self.navigationController!.view, animated: true)
-                hud.mode = MBProgressHUDMode.annularDeterminate
-                hud.label.text = "Downloading IITC script..."
-
-                Alamofire.download("https://iitc.me/build/test/total-conversion-build.user.js", to: {
-                    _, _ -> (destinationURL: URL, options: DownloadRequest.DownloadOptions) in
-                    let downloadPath = ScriptsManager.sharedInstance.userScriptsPath.appendingPathComponent("total-conversion-build.user.js")
-                    return (downloadPath, DownloadRequest.DownloadOptions.removePreviousFile)
-                }).downloadProgress {
-                    progress in
-
-                    // This closure is NOT called on the main queue for performance
-                    // reasons. To update your ui, dispatch to the main queue.
-                    hud.progressObject = progress
-                }.response {
-                    downloadResponse in
-
-                    hud.hide(animated: true)
-                    if downloadResponse.error != nil {
-                        let alert1 = UIAlertController(title: "Error", message: downloadResponse.error!.localizedDescription, preferredStyle: .alert)
-                        alert1.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                        self.present(alert1, animated: true, completion: nil)
-                    }
-                }
-
-            }))
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-            self.present(alert, animated: true, completion: nil)
-
-        } else if specifier.key() == "pref_console_not_purchased" {
-            guard let vc = self.navigationController?.storyboard?.instantiateViewController(withIdentifier: "purchase") else {
-                return
-            }
-            vc.modalPresentationStyle = UIModalPresentationStyle.formSheet
-            self.present(vc, animated: true, completion: nil)
-        } else if specifier.key() == "pref_useragent_button" {
-            let vc = self.navigationController!.storyboard!.instantiateViewController(withIdentifier: "userAgentViewController")
-            self.navigationController!.pushViewController(vc, animated: true)
-        }
     }
 
     func settingsViewControllerDidEnd(_ sender: IASKAppSettingsViewController!) {
