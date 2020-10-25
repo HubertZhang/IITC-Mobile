e944246c43d18fe75d99c32930883af89844ec2b
Hubert Zhang <hubert.zyk@gmail.com>
2020-10-26 03:39:12 +0800

    Add user agent of iOS 14.1

--- BaseFramework/Constants.swift
+++ BaseFramework/Constants.swift
@@ -11,6 +11,8 @@ import UIKit
 public let ContainerIdentifier: String = "group.com.vuryleo.iitc"
 
 public let PredefinedUserAgents: [(String, String)] = [
+    ("Safari on iOS 14.1", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"),
+    ("Chrome 86 on iOS 14.1","Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/86.0.4240.93 Mobile/15E148 Safari/604.1"),
     ("Safari on iOS 13.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"),
     ("Chrome 78 on iOS 13.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/78.0.3904.84 Mobile/15E148 Safari/604.1"),
     ("Safari on iOS 12.3.1", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/604.1"),
