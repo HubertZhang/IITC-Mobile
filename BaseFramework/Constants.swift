077040dffa4f8f83544d70773672b0314ce2a48c
Hubert Zhang <hubert.zyk@gmail.com>
2021-10-21 01:45:00 +0800

    add useragent for ios 15

--- BaseFramework/Constants.swift
+++ BaseFramework/Constants.swift
@@ -11,10 +11,10 @@ import UIKit
 public let ContainerIdentifier: String = "group.com.vuryleo.iitc"
 
 public let PredefinedUserAgents: [(String, String)] = [
+    ("Safari on iOS 15.0.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"),
+    ("Chrome 94 on iOS 15.0.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/94.0.4606.76 Mobile/15E148 Safari/604.1"),
     ("Safari on iOS 14.4", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1"),
     ("Chrome 87 on iOS 14.4", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1"),
     ("Safari on iOS 14.1", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"),
     ("Chrome 86 on iOS 14.1", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/86.0.4240.93 Mobile/15E148 Safari/604.1"),
-    ("Safari on iOS 13.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"),
-    ("Chrome 78 on iOS 13.2", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/78.0.3904.84 Mobile/15E148 Safari/604.1")
 ]
